efbf7679822657d46544a8d9a5cc036a92d15be9
mucidaa <alvaromucidaac@gmail.com>
2025-05-05 14:42:56 -0300

    Primeiro controller criado

---
+++ src/main/java/com/mucida/study/Controller.java
@@ -0,0 +1,17 @@
+package com.mucida.study;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+
+    @GetMapping
+    public String home(){
+        return "This is the home page";
+    }
+
+}
