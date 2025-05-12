f7e3bc16438967158f9a24706e9a585bc3f97c52
mucidaa <alvaromucidaac@gmail.com>
2025-05-12 14:53:56 -0300

    build: Conecta o banco de dados H2 à aplicação

--- src/main/java/com/mucida/study/repository/MissaoRepository.java
+++ src/main/java/com/mucida/study/repository/MissaoRepository.java
@@ -1,7 +1,6 @@
 package com.mucida.study.repository;
 
 import com.mucida.study.model.MissaoModel;
-import com.mucida.study.model.NinjaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
