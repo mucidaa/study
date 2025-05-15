fcd0b846c3eaf3b358c9b1b17bd8bb5cd9d07dac
mucidaa <alvaromucidaac@gmail.com>
2025-05-15 14:59:27 -0300

    feat: Atualiza o MissaoController com endpoints CRUD

--- src/main/java/com/mucida/study/controller/MissaoController.java
+++ src/main/java/com/mucida/study/controller/MissaoController.java
@@ -1,4 +1,34 @@
 package com.mucida.study.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissaoController {
+
+    @PostMapping
+    public String addMissao(){
+        return "Missao added";
+    }
+
+    @GetMapping
+    public String getAllMissoes(){
+        return "All Missoes got";
+    }
+
+    @GetMapping("/{id}")
+    public String getMissaoById(@PathVariable int id){
+        return "Missao got";
+    }
+
+    @PutMapping
+    public String updateMissao(){
+        return "Missao updated";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteMissao(@PathVariable int id){
+        return "Missao deleted";
+    }
+
 }
