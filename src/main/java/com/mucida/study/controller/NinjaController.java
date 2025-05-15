f89a2d988e0484d9f8de432dca9172b363d0240b
mucidaa <alvaromucidaac@gmail.com>
2025-05-15 14:34:17 -0300

    feat: Atualiza o Controller com mock de dados

--- src/main/java/com/mucida/study/controller/NinjaController.java
+++ src/main/java/com/mucida/study/controller/NinjaController.java
@@ -1,17 +1,39 @@
 package com.mucida.study.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
 
-
     @GetMapping
     public String home(){
         return "This is the home page";
+    }
+
+    @PostMapping("/ninjas")
+    public String addNinja(){
+        return "Ninja added";
+    }
+
+    @GetMapping("/ninjas")
+    public String getAllNinjas(){
+        return "All ninjas got";
+    }
+
+    @GetMapping("/ninjas/{id}")
+    public String getNinjaById(@PathVariable int id){
+        return "Ninja got";
+    }
+
+    @PutMapping("/ninjas")
+    public String updateNinja(){
+        return "Ninja updated";
+    }
+
+    @DeleteMapping("/ninjas/{id}")
+    public String deleteNinja(@PathVariable int id){
+        return "Ninja deleted";
     }
 
 }
