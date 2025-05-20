a962034a6f3c7ba35f8451e950c05613a85f22bf
mucidaa <alvaromucidaac@gmail.com>
2025-05-20 13:56:20 -0300

    feat: Cria o DTO para ninja

--- src/main/java/com/mucida/study/model/DTO/NinjaDTO.java
+++ src/main/java/com/mucida/study/model/DTO/NinjaDTO.java
@@ -1,4 +1,20 @@
-package com.mucida.study.model.DTO;
+package com.mucida.study.model.dto;
 
+import com.mucida.study.model.MissaoModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private long id;
+    private String name;
+    private String email;
+    private int idade;
+    private String rank;
+    private MissaoModel missao;
+
 }
